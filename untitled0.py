21818c22b8962aea0f88f5dfc3224939dfae193b
t06104421998 <56292886+t06104421998@users.noreply.github.com>
2019-10-14 19:45:33 +0800

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,23 @@
+i=1100
+j=12.3
+k=True
+print(i,j,k)
+del k
+
+
+print(1+2)
+print('abc'*2)
+print(6/3)
+print(10//3)
+print(28%15)
+print(5**10)
+
+
+print(123=='123')
+print('ABC'=='abc')
+print(True==1)
+print(False==0)
+print(3>5==True)
+print(3>2 and 2>3)
+print(3>2 or 2>3)
+print(not 3<2)
